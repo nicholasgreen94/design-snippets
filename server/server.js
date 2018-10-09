3a99f1805e7c57b681712cccb48531ac628194b8
Nicholas Green <nicholasgreen94@gmail.com>
2018-10-09 13:54:09 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const path = require('path')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true, }))
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+const PORT = process.env.PORT || 3001
+// Send Request
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Now connected on port ${PORT}`)
+})
