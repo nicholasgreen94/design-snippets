9a355dbfd19a94f2f7e4d71c5a0e7d075c2895b5
Nicholas Green <nicholasgreen94@gmail.com>
2018-10-09 20:52:46 -0600

    set up app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>designsnippet</title>
+            <meta name="description" content="designsnippet" />
+            <meta http-equiv="x-ua-compatible" content="ie=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Helmet>
+          <a href='#skip' id='skip-nav'>{"Skip Nav"}</a>
+          <div id='site-content'>
+            <a id='skip'>{''}</a>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
